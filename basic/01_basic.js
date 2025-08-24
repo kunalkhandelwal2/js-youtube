c04f7cbf181710bf6e8a69ae2c33b08dec37f899
Kunal Khandelwal <115810044+kunalkhandelwal2@users.noreply.github.com>
2025-08-24 14:20:29 +0000

    container config done

--- basic/01_basic.js
+++ basic/01_basic.js
@@ -1,1 +1,16 @@
-console.log("kunal khandelwal")
+// console.log("kunal khandelwal")
+
+const account=85885 // doesnt change after decleration
+let acc="kunal"   // mainly use let for variable decleartion 
+var mail="kk@google.com" // not use beacuse var dont the scope system means he can assume inside {} variable outside variables same way thats a problem for code early days thats why replace with let
+kk="hero"// no let or var use but this one also valid because js automatically create memory for these with his featuers and also automatically check which data type
+console.table([account,acc,mail]) // this one use for run multiple variable in one time to save the time and efficiency
+
+
+//command prompt
+/* 1. pwd : to check the directory means line by line how many folder inside these file have.
+  2. ls: how many file have in these folder and which one is open in present by some extre colour
+  */
+ // ctrl+/(backspace):shortcut for one line comment for all programming langauge
+ let accstate; // thats away we can declare a variable in js and output in undefiend beacuse no value give in memory undefined store means garbage
+ console.log(accstate) // o/p undefined
