1f5a55e5251d87de0066dd37ea8e637c2746bf8c
Kunal Khandelwal <115810044+kunalkhandelwal2@users.noreply.github.com>
2025-08-25 10:49:24 +0000

    datatype

---
+++ basic/02_basic.js
@@ -0,0 +1,19 @@
+"use strict"//these means treat all code new version of javascript because 8 years ago and today full difference of featuers and code
+// automatically many time js treat as a newer version automatically
+// data type
+//primitive data types
+let kk=988 //int data type
+let kkk=883673737//bigint data type for big values like stocks values many line number occur
+let name="hero"//string data type
+let isloggedin=true //boolean data type true oe false
+let only;// undefined data type means variable deleare and memory created but value npt assign
+let onlyy=null//null data type means standalone value like when we fetch the weather value then null cam means zero degree thats why many time prefer null value
+// symbol data type : to know uniqueness use in reactjs max time
+
+// non-primitve data type
+//1.object
+
+// typeof to know the type of value
+console.log(typeof onlyy) // main reason to show you null typeof because 'null' type show js 'object' flaw have oe anything dont know
+console.log(typeof only)// undefined type also undefined
+console.log(typeof name)// strign type 
